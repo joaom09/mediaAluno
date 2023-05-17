8e6dce4ada75e1da6cf7533a7dfa0dca1da381d4
JOÃO MARCOS SIQUEIRA RODRIGUES <00776630202@IDARON.GOV>
2023-05-17 12:03:47 -0400

    update to fix errors

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 function media() {
     var nomeAluno = document.getElementById("Nome").value;
     var numeroMatricula = document.getElementById("Matricula").value;
-
-    var nota1 = parseFloat(document.getElementById("N1").value);
+    
+    var nota1 = parseFloat(document.getElementById("N1").value); 
+    if (nota1 >=0 && nota1<=10) {
+       console.log("Nota válida");
+    } else{
+        console.log("Nota inválida");
+        alert("Insira uma nota entre 0 a 10");
+        nota1 = undefined;
+    }
+    
     var nota2 = parseFloat(document.getElementById("N2").value);
-
+    if (nota2 >=0 && nota2<=10) {
+        console.log("Nota válida");
+     } else{
+         console.log("Nota inválida");
+         alert("Insira uma nota entre 0 a 10");
+         nota2 = undefined;
+     }
     var notaFinal = (nota1 + nota2) / 2;
 
     document.getElementById("apresentaNotas").textContent = "O aluno " + nomeAluno + ", matrícula " + numeroMatricula + ", obteve a média final " + notaFinal;
